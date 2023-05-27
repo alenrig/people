78ef24c03ece620b7beb1d820f6ccb7141e5aca3
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-27 19:06:42 +0300

    Black src

--- src/people/models.py
+++ src/people/models.py
@@ -6,19 +6,17 @@ db = SqliteDatabase("people.db")
 
 
 class BaseModel(Model):
-    """Parent class for tables ORM.
+    """Parent class for tables ORM."""
 
-    Args:
-        Model
-    """
     class Meta:
         database = db
 
 
 class People(BaseModel):
-    """DB Table for people representation.
+    """Table for people representation.
 
     Args:
         BaseModel: parent class with meta definition.
     """
+
     name = CharField()
