1ec1875f009ba6928602502ad08eecb824288e89
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-27 10:55:34 +0300

    Black src

--- src/people/__init__.py
+++ src/people/__init__.py
@@ -3,6 +3,6 @@ from .__main__ import people
 from .models import People
 
 People.create_table()
-commands = [add,ls]
+commands = [add, ls]
 for command in commands:
     people.add_command(command)
