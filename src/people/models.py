0245ec142e4b6eea5943a4ea56007f1b3d4c71cf
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-27 14:51:47 +0000

    Add docs for models module

--- src/people/models.py
+++ src/people/models.py
@@ -1,12 +1,24 @@
+"""Module for ORM modules."""
+
 from peewee import CharField, Model, SqliteDatabase
 
 db = SqliteDatabase("people.db")
 
 
 class BaseModel(Model):
+    """Parent class for tables ORM.
+
+    Args:
+        Model
+    """
     class Meta:
         database = db
 
 
 class People(BaseModel):
+    """DB Table for people representation.
+
+    Args:
+        BaseModel: parent class with meta definition.
+    """
     name = CharField()
