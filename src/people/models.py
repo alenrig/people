615e5e1b42e78a99bc196af97197b9c1f76cfda1
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-27 19:11:18 +0300

    Fix models module

--- src/people/models.py
+++ src/people/models.py
@@ -1,5 +1,7 @@
 """Module for ORM modules."""
 
+from dataclasses import dataclass
+
 from peewee import CharField, Model, SqliteDatabase
 
 db = SqliteDatabase("people.db")
@@ -8,10 +10,14 @@ db = SqliteDatabase("people.db")
 class BaseModel(Model):
     """Parent class for tables ORM."""
 
+    @dataclass
     class Meta:
+        """Class for meta information defining."""
+
         database = db
 
 
+@dataclass
 class People(BaseModel):
     """Table for people representation.
 
