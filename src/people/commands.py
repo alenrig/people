717ca64ed26c7f017c42f4f4f9ecf9cdcd4928eb
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-28 10:30:59 +0300

    Black src

--- src/people/commands.py
+++ src/people/commands.py
@@ -10,19 +10,17 @@ from .utils.date_formatter import date_formatter
 @click.command(name="ls")
 def list_people() -> None:
     """List contacts."""
-    data = [[f"{man.first_name} {man.last_name}", man.last_contacted] for man in People.select()]
+    data = [
+        [f"{man.first_name} {man.last_name}", man.last_contacted]
+        for man in People.select()
+    ]
     print_table(["Name", "Last Contacted"], data)
 
 
 @click.command
 @click.argument("first_name", type=str)
 @click.argument("last_name", type=str)
-@click.option(
-    "-d",
-    "--date",
-    default=date.today,
-    help="Last contacted date"
-)
+@click.option("-d", "--date", default=date.today, help="Last contacted date")
 def add(first_name: str, last_name: str, date: str) -> None:
     date = date_formatter(date)
     People.create(first_name=first_name, last_name=last_name, last_contacted=date)
--- src/people/utils/date_formatter.py
+++ src/people/utils/date_formatter.py
@@ -4,4 +4,4 @@ from dateutil import parser
 
 def date_formatter(string: str) -> date:
     dt = parser.parse(string)
-    return datetime.strptime(f"{dt.year}-{dt.month}-{dt.day}", '%Y-%m-%d').date()
+    return datetime.strptime(f"{dt.year}-{dt.month}-{dt.day}", "%Y-%m-%d").date()
