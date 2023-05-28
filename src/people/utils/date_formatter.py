67c979a4d299d883eec10693e5f06e853522f031
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-28 10:56:28 +0300

    Fix lint errors for date formatter

--- src/people/utils/date_formatter.py
+++ src/people/utils/date_formatter.py
@@ -1,6 +1,15 @@
+"""Module for working with dates."""
 from dateutil import parser
 
 
 def date_formatter(date: str) -> str:
-    dt = parser.parse(date)
-    return f"{dt.year}-{dt.month}-{dt.day}"
+    """Formate date in YYYY-mm-dd format.
+
+    Args:
+        date (str): datetime in any format.
+
+    Returns:
+        str: date in YYYY-mm-dd format.
+    """
+    parsed_date = parser.parse(date)
+    return f"{parsed_date.year}-{parsed_date.month}-{parsed_date.day}"
