3470ef90616e042cad856653523fa11e9c694f22
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-05-28 10:51:38 +0300

    Add package for mypy

--- src/people/utils/date_formatter.py
+++ src/people/utils/date_formatter.py
@@ -3,6 +3,6 @@ from datetime import date, datetime
 from dateutil import parser
 
 
-def date_formatter(string: str) -> date:
-    dt = parser.parse(string)
-    return datetime.strptime(f"{dt.year}-{dt.month}-{dt.day}", "%Y-%m-%d").date()
+def date_formatter(date: str) -> str:
+    dt = parser.parse(date)
+    return f"{dt.year}-{dt.month}-{dt.day}"
