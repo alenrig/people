51838a117c58cf1cc37f62b364165bf3383de66c
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-06-24 23:35:20 +0300

    Black src

--- src/people/__init__.py
+++ src/people/__init__.py
@@ -1,10 +1,10 @@
 """Package init module."""
 
-from .commands import add, list_people, remove, contact
+from .commands import add_command, list_people_command, remove_command, contact_command
 from .__main__ import people
 from .models import People
 
 People.create_table()
-commands = [add, list_people, remove, contact]
+commands = [add_command, list_people_command, remove_command, contact_command]
 for command in commands:
     people.add_command(command)
