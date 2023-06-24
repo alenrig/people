2f9f320806be83d075ac7a8e0c66584229d649c8
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-06-25 00:29:27 +0300

    Fix

--- src/people/helpers/remove.py
+++ src/people/helpers/remove.py
@@ -1,13 +1,4 @@
-from datetime import date
-from typing import List, Optional, Union
-
-from peewee import DateField
-
-from ..__main__ import SHORT_TABLE_HEADER
 from ..models import People
-from ..utils.data_formatter import set_in_rows
-from ..utils.dates import date_formatter
-from ..utils.table import print_table
 
 
 def remove(name: str, surname: str) -> None:
