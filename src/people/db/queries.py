15aa63b6c77ebb7046d3ef254bbd72911f24c4bc
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-06-25 11:18:45 +0300

    Black src

--- src/people/db/queries.py
+++ src/people/db/queries.py
@@ -12,15 +12,15 @@ def add_person_to_db(
     surname: str, name: Optional[str] = None, last_contact_date: str = str(date.today())
 ) -> People:
     _is_already_exists(surname, name)
-    result: People = People.create(name=name, surname=surname, last_contact=last_contact_date)
+    result: People = People.create(
+        name=name, surname=surname, last_contact=last_contact_date
+    )
     return result
 
 
 def _is_already_exists(surname: str, name: Optional[str] = None):
     people = None
     with contextlib.suppress(DoesNotExist):
-        people = People.get(
-            People.name == name and People.surname == surname
-        )
+        people = People.get(People.name == name and People.surname == surname)
     if people:
         sys.exit("Person already exists.")
