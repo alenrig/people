3c4ee807de3e0672e7715661f4e78abdfb89c766
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-06-26 21:16:03 +0300

    Add config init

--- src/people/__init__.py
+++ src/people/__init__.py
@@ -1,10 +1,26 @@
 """Package init module."""
+import configparser
+import contextlib
+import os
 
 from .__main__ import people
 from .db.models import People
 
 # isort: off
 from .commands import add_command, contact_command, list_people_command, remove_command
+
+config = configparser.ConfigParser()
+configdir = f"{os.getenv('HOME')}/.config/people"
+configpath = f"{configdir}/config.ini"
+if not os.path.isfile(configpath):
+    with contextlib.suppress(OSError):
+        os.makedirs(configdir)
+    config.add_section("main")
+    config.set("main", "config_dir", configdir)
+    config.add_section("database")
+    config.set("database", "file", "people.db")
+    with open(configpath, "w") as file:
+        config.write(file)
 
 People.create_table()
 commands = [add_command, list_people_command, remove_command, contact_command]
