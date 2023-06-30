e32ab6ffb02da57070065999d248cc8a9595c2d4
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-06-30 23:34:27 +0300

    Add test for readd person check

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,13 +1,27 @@
-from people.db.queries import add_person_to_db, delete_person_from_db
+from people.db.queries import add_person_to_db
 import pytest
 
 @pytest.mark.parametrize(
     "surname, name, last_contact_date, expected",
     [
         ("surname1", "name1", "27.06.2023", ("surname1", "name1", "27.06.2023")),
-        ("surname2", "name2", "11.11.2011", ("surname2", "name2", "11.11.2011"))
+        ("surname2", None, "11.11.2011", ("surname2", None, "11.11.2011"))
     ]
 )
 def test_add_person_to_db(test_db, surname, name, last_contact_date, expected):
     result = add_person_to_db(surname, name, last_contact_date)
     assert result.surname == expected[0]
+    assert result.name == expected[1]
+    assert result.last_contact == expected[2]
+
+
+@pytest.mark.parametrize(
+    "surname, name",
+    [
+        ("surname1", "name1"),
+    ]
+)
+def test_readding_person_to_db(test_db, surname, name):
+    add_person_to_db(surname, name)
+    with pytest.raises(SystemExit):
+        add_person_to_db(surname, name)
