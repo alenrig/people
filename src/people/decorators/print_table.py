0e20d5fb42f16b5f849d526ddeed8c18db66255e
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-07-22 15:10:59 +0000

    Add docs

--- src/people/decorators/print_table.py
+++ src/people/decorators/print_table.py
@@ -1,8 +1,15 @@
+"""Module for print table wrapper."""
 from ..configs import SHORT_TABLE_HEADER, TABLE_HEADER
 from ..utils.table import print_table
 
 
 def print_table_wrapper(func):
+    """Print pretty table after comand execution.
+
+    Args:
+        func (__func__): command of people cli to run.
+    """
+
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         try:
