3704386397544499c571be159e7478fe7e6ef1b4
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-07-23 21:48:05 +0300

    Rewrite contact command

--- src/people/helpers/contact.py
+++ src/people/helpers/contact.py
@@ -4,14 +4,16 @@ from typing import List, Optional, Union
 
 from peewee import DateField
 
-from ..configs import SHORT_TABLE_HEADER
 from ..db.queries import update_last_contact_date
+from ..decorators.print_table import print_table_wrapper
 from ..utils.data_formatter import set_in_rows
 from ..utils.dates import date_formatter
-from ..utils.table import print_table
 
 
-def contact(surname: str, name: Optional[str], last_contact: str) -> None:
+@print_table_wrapper
+def contact(
+    surname: str, name: Optional[str], last_contact: str
+) -> List[List[Union[str, DateField]]]:
     """Update person last contact date.
 
     Args:
@@ -19,13 +21,6 @@ def contact(surname: str, name: Optional[str], last_contact: str) -> None:
         name (Optional[str]): person name.
         last_contact (str): last contact date with person.
     """
-    people = _contact(surname, name, last_contact)
-    print_table(SHORT_TABLE_HEADER, people)
-
-
-def _contact(
-    surname: str, name: Optional[str], last_contact: str
-) -> List[List[Union[str, DateField]]]:
     last_contact_date: date = date_formatter(last_contact)
     person = update_last_contact_date(surname, name, last_contact_date)
     return set_in_rows([person], passed_days=False)
